19e760cf6063adf5df748d179faca8e974cb1776
Shilpa Garg <sgarg@PUNsgarg5LJG5J.local>
2020-09-30 16:53:43 +0530

    First Git Commint for practice

---
+++ src/main/java/com/qa/libs/Testbase.java
@@ -0,0 +1,15 @@
+package com.qa.libs;
+
+public class Testbase {
+
+	
+		
+         
+		public void Test()
+		{
+			System.out.println("Test Method");
+		}
+
+	
+
+}
---
+++ src/main/java/com/qa/utitlities/TestUtil.java
@@ -0,0 +1,12 @@
+package com.qa.utitlities;
+
+public class TestUtil {
+	
+
+	public void testUtil()
+	{
+		
+		System.out.println("Test Util");
+	}
+
+}
